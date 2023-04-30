5b3860f982aac603c5ba6cbbf4fcfe66bfd1bfd7
haixie <haixie@student.unimelb.edu.au>
2023-04-30 22:58:39 +1000

    before generate transId from timestamp

--- cmd/Replica/StateMachine.go
+++ cmd/Replica/StateMachine.go
@@ -2,7 +2,9 @@ package Replica
 
 import (
 	pb "Distributed_Key_Value_Store/cmd/Primitive"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
+	"time"
 )
 
 type Transaction struct {
@@ -13,14 +15,38 @@ type TimeoutTask struct {
 	trans Transaction
 }
 type StateMachine struct {
-	id      int           // Actual node will map the Id to actual address
+	id      int32         // Actual node will map the Id to actual address
 	m_trans []Transaction // Managed transactions, because this state machine is its coordinator
 	//TODO if need optimization of its data structure
 	w_trans []Transaction // witnessed transactions
 }
 
-func (st *StateMachine) sendPreAccept(tars []int) []*pb.Message {
+func (st *StateMachine) generateTimestamp() *pb.TransTimestamp {
+	timeNow := time.Now()
+	t0 := &pb.TransTimestamp{
+		TimeStamp: &timestamppb.Timestamp{
+			Seconds: timeNow.Unix(),
+			Nanos:   int32(timeNow.Nanosecond()),
+		},
+		Seq: 0,
+		Id:  st.id,
+	}
+	return t0
+}
+func (st *StateMachine) generateTransId(transTimestamp *pb.TransTimestamp) *string {
 
+}
+
+func (st *StateMachine) sendPreAccept(tars []int, trans *pb.Trans) []*pb.Message {
+	var msgs []*pb.Message
+	t0 := st.generateTimestamp()
+	preAccept := pb.PreAcceptReq{
+		Trans: trans,
+		T0:    t0,
+	}
+	for i := 0; i < len(tars); i++ {
+
+	}
 }
 
 func (st *StateMachine) executeReq(req *pb.Message) []*pb.Message {
