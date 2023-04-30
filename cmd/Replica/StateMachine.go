523ecb9cb5de75f2e2862669b76fcce25d6ae93e
haixie <haixie@student.unimelb.edu.au>
2023-04-30 14:44:56 +1000

    Add replica

--- cmd/Replica/StateMachine.go
+++ cmd/Replica/StateMachine.go
@@ -1,1 +1,19 @@
 package Replica
+
+import (
+	pb "Distributed_Key_Value_Store/cmd/Primitive"
+)
+
+type Transaction struct {
+	in_trans pb.Trans
+}
+type TimeoutTask struct {
+	fc    func(Transaction)
+	trans Transaction
+}
+type StateMachine struct {
+	id      int           // Actual node will map the Id to actual address
+	m_trans []Transaction // Managed transactions, because this state machine is its coordinator
+	//TODO if need optimization of its data structure
+	w_trans []Transaction // witnessed transactions
+}
